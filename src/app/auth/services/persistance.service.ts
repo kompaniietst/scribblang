965e77e0335c54c30620ac680d8bf1bd01ce10aa
kompaniietst <kompaniets.t@gmail.com>
2021-12-14 05:55:37 +0200

    persistance service

---
+++ src/app/auth/services/persistance.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class PersistanceService {
+    set(key: string, data: string): void {
+        const processedData = JSON.stringify(data);
+
+        window.localStorage.setItem(key, processedData);
+    }
+
+    get(key: string): string {
+        const data = window.localStorage.getItem(key);
+
+        return JSON.parse(data);
+    }
+}

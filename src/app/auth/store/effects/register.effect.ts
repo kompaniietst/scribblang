9475f2e70fb447ac79c5c229099b129ee67342cb
kompaniietst <kompaniets.t@gmail.com>
2021-12-14 20:05:03 +0200

    moving to login page after registration

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { registerAction, registerFailAction, registerSuccessAction } from "../actions/register.action";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { of } from "rxjs";
 import { AuthService } from "../../services/auth.service";
 import { CurrentUserInterface } from "../../interfaces/currentUser.interface";
 import { BackendErrorsInterface } from "../../interfaces/backendErrors.interface";
 import { HttpErrorResponse } from "@angular/common/http";
 import { PersistanceService } from "../../services/persistance.service";
+import { Router } from "@angular/router";
+import { dispatch } from "rxjs/internal/observable/pairs";
 
 @Injectable()
 export class RegisterEffect {
@@ -31,6 +33,12 @@ export class RegisterEffect {
             })))
 
     constructor(private actions$: Actions, private authService: AuthService,
-        private persistanceService: PersistanceService) { }
+        private persistanceService: PersistanceService, private router: Router) { }
+
+    redirectAfterSubmit$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(registerSuccessAction),
+            tap(() => this.router.navigateByUrl("/login"))
+        ), { dispatch: false })
 }
 
